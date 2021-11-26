497fbc583b8f714c3af8cd65ef0237b2966a698e
SergeiSSH <81880270+SergeiSSH@users.noreply.github.com>
2021-11-26 20:31:09 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -42,29 +42,21 @@ Lamp.prototype.getNominal = function () {
     return `Описание лампы: ${this.type}, color: ${this.color}`;
 }
 
-let pc1 = new PC(40000,2,220,'AMD Ryzen 5 3600','A-Data XPG 32G','A-Data XPG 512G','Radeon 6600XT');
-let lamp1 = new Lamp(2800,1,220,'floor lamp','gold');
-
-
 Appliance.prototype.On = function () {
     this.switch = false
+    console.log('прибор включен.')
 }
 Appliance.prototype.Off = function () {
     this.switch = true
+    console.log('прибор выключен.')
 }
 
 
-
+let pc1 = new PC(40000,2,220,'AMD Ryzen 5 3600','A-Data XPG 32G','A-Data XPG 512G','Radeon 6600XT');
+let lamp1 = new Lamp(2800,1,220,'floor lamp','gold');
 
 console.log(pc1.getNominal());
 pc1.On();
-if (pc1.switch) {
-    console.log(pc1.getNominal()+' выключен.');
-}else {
-    pc1.getNominal();
-    console.log(pc1.getNominal()+' включен.');
-}
-
 pc1.getPrice();
 pc1.getWatt();
 
@@ -72,11 +64,5 @@ console.log('****************')
 
 console.log(lamp1.getNominal());
 lamp1.Off();
-if (lamp1.switch) {
-    console.log(lamp1.getNominal()+' выключена.');
-}else {
-    console.log(lamp1.getNominal()+' включена.');
-}
-
 lamp1.getPrice();
 lamp1.getWatt();
